fa0bf9bbf32d6f6eedf9ad1f7e011d1227597237
geicibarham <geicianecosta43@gmail.com>
2023-01-11 18:14:28 -0500

    function now generates password

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
-
 const form = document.getElementById("form");
-const passlength=document.getElementById('length')
-const handleSubmit = (e) => {
-   e.preventDefault();
-   location.reload;
-   console.log(passlength.value)
- };
+const passlength = document.getElementById("length");
 
- form.addEventListener('submit',handleSubmit)
+let password = "";
+let passwordText = "";
+
+var numbers = '0123456789'
+const alphabetupper = "ABCDFGHIJKLMNOPQRSTUWXYZ";
+var specialCharacters = '()${}@!*#%&'
+const lowerCase = "abdcefghijklmnopqrsteuvxz";
+const randNum = Math.floor(Math.random() * 10);
+
+
+let len;
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  len = Number(passlength.value);
+  generatePassword();
+
+};
+
+const generatePassword = () => {
+  if (document.getElementById("uppercase").checked) {
+    password = password += alphabetupper;
+  }
+
+  if (document.getElementById("special").checked) {
+    password = password += specialCharacters;
+  }
+
+  if (document.getElementById("lowercase").checked) {
+    password = password += lowerCase;
+  }
+
+  if (document.getElementById("numbers").checked) {
+    password = password += numbers;
+  }
+
+  // console.log(password);
+
+  // // console.log(passwordText)
+
+  for (var i = 0; i < len; i++) {
+    var randomIndex = Math.floor(Math.random() * password.length);
+    var randomCharacter = password[randomIndex];
+    passwordText += randomCharacter;
+  }
+
+
+  var pass= document.querySelector("#password");
+  pass. innerHTML= passwordText;
+};
+
+
+
+form.addEventListener("submit", handleSubmit);
