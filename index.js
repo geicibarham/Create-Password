ae4a3ec60c934a3baf286b80c319c55abf28f3a1
geicibarham <geicianecosta43@gmail.com>
2023-01-11 20:38:58 -0500

    adding possibility to copy and paste

--- index.js
+++ index.js
@@ -1,57 +1,68 @@
 const form = document.getElementById("form");
 const passlength = document.getElementById("length");
+const passwordcontainer = document.getElementById("password-container");
 
+let p = document.getElementById("p");
 let password = "";
 let passwordText = "";
 
-var numbers = '0123456789'
+let numbers = "0123456789";
 const alphabetupper = "ABCDFGHIJKLMNOPQRSTUWXYZ";
-var specialCharacters = '()${}@!*#%&'
+let specialCharacters = "()${}@!*#%&";
 const lowerCase = "abdcefghijklmnopqrsteuvxz";
 const randNum = Math.floor(Math.random() * 10);
 
-
+let btn;
 let len;
 
 const handleSubmit = (e) => {
   e.preventDefault();
   len = Number(passlength.value);
   generatePassword();
-
 };
 
 const generatePassword = () => {
-  if (document.getElementById("uppercase").checked) {
-    password = password += alphabetupper;
+  if (len > 4 || len > 32) {
+    if (document.getElementById("uppercase").checked) {
+      password = password += alphabetupper;
+    }
+
+    if (document.getElementById("special").checked) {
+      password = password += specialCharacters;
+    }
+
+    if (document.getElementById("lowercase").checked) {
+      password = password += lowerCase;
+    }
+
+    if (document.getElementById("numbers").checked) {
+      password = password += numbers;
+    }
+
+    for (let i = 0; i < len; i++) {
+      let randomIndex = Math.floor(Math.random() * password.length);
+      let randomCharacter = password[randomIndex];
+      passwordText += randomCharacter;
+    }
+
+    p.innerHTML = passwordText;
+    btn = document.createElement("button");
+    btn.innerHTML = "Copy";
+    passwordcontainer.classList.add("passwordContainer");
+
+    passwordcontainer.appendChild(btn);
+    btn.addEventListener("click", copyContent);
   }
-
-  if (document.getElementById("special").checked) {
-    password = password += specialCharacters;
-  }
-
-  if (document.getElementById("lowercase").checked) {
-    password = password += lowerCase;
-  }
-
-  if (document.getElementById("numbers").checked) {
-    password = password += numbers;
-  }
-
-  // console.log(password);
-
-  // // console.log(passwordText)
-
-  for (var i = 0; i < len; i++) {
-    var randomIndex = Math.floor(Math.random() * password.length);
-    var randomCharacter = password[randomIndex];
-    passwordText += randomCharacter;
-  }
-
-
-  var pass= document.querySelector("#password");
-  pass. innerHTML= passwordText;
 };
 
-
+const copyContent = async () => {
+  console.log(p.innerHTML);
+  try {
+    await navigator.clipboard.writeText(p.innerHTML);
+    console.log("Content copied to clipboard");
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
+};
 
 form.addEventListener("submit", handleSubmit);
